bf6d751c3f7217b55772878442b6365cc6313d22
Miguel Fernandez <miguelferlez@gmail.com>
2025-06-12 14:27:19 +0200

    refactor: change commonjs to module syntax

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from "express";
+import path from "node:path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import createError from "http-errors";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/**
+ * Catch 404 and forward to error handler
+ */
 
-module.exports = app;
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+/**
+ * Error handler
+ */
+
+app.use(function (err, req, res, next) {
+  if (err.array) {
+    err.message =
+      "Invalid request: " +
+      err
+        .array()
+        .map(
+          (error) =>
+            `${error.location} ${error.type} ${error.path} ${error.msg}`
+        )
+        .join(", ");
+    err.status = 422;
+  }
+
+  res.status(err.status || 500);
+
+  //TODO: json response for /api/ errors
+
+  res.locals.message = err.message;
+  res.locals.error = process.env.APP_ENV === "development" ? err : {};
+
+  res.render("error");
+});
+
+export default app;
---
+++ bin/server.js
@@ -0,0 +1,39 @@
+import "dotenv/config";
+import http from "node:http";
+import app from "../app.js";
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = process.env.PORT || 3000;
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  console.log(error);
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  console.log(`Server started on http://localhost:${port}\n`);
+}
