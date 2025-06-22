a205123e7d459232f870869272138d0827856e2a
Miguel Fernandez <miguelferlez@gmail.com>
2025-06-22 13:18:35 +0200

    style: render product thumbnail image if exists

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,4 +1,5 @@
 import Product from "../models/Product.js";
+import { existsSync } from "node:fs";
 
 export async function index(req, res, next) {
   try {
@@ -17,6 +18,17 @@ export async function index(req, res, next) {
 
     const products = await Product.list(filter, limit, skip, sort);
     const total = await Product.find(filter).countDocuments();
+
+    for (let product of products) {
+      if (product.image) {
+        const thumbnail = product.image.replace(/\.(?=[^.]+$)/, "_thumbnail.");
+        if (existsSync(`public/images/${thumbnail}`)) {
+          product.image = thumbnail;
+        }
+      } else {
+        product.image = "product-placeholder.jpg";
+      }
+    }
 
     res.locals.products = products;
     res.locals.username = req.session.username;
